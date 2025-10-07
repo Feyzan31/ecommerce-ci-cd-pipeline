41f38f71c055ff9976cc6b5606b108e7bb7845b2
souad <souadbouzgaou7@gmail.com>
2025-10-07 16:47:05 +0100

    rectifier une erreur sur les tests

--- frontend/src/__test__/Admin.test.jsx
+++ frontend/src/__test__/Admin.test.jsx
@@ -35,7 +35,7 @@ describe("Admin dashboard", () => {
 
   test("supprime un produit", async () => {
     render(<Admin />);
-    const deleteBtn = await screen.findByRole("button", { name: i });
+    const deleteBtn = await screen.findByRole("button", { name: /🗑️/i });
     await userEvent.click(deleteBtn);
 
     await waitFor(() => {
